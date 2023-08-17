37b47e39299c0e5f1b6838ebd984190b277f7b7e
Gamber-github <wojciech.kubik1993@gmail.com>
2023-08-17 08:44:28 +0200

    Libraries added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,53 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "prettier",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react-refresh", "import", "prettier", "@tanstack/query"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": "warn",
+    semi: ["error", "never"],
+    quotes: ["error", "double"],
+    "@typescript-eslint/no-explicit-any": 2,
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "newline-before-return": 2,
+    "padding-line-between-statements": ["error", { blankLine: "always", prev: "*", next: "export" }],
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
   },
 }
