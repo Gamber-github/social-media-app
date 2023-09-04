bd475779e74c2931cef5542a25863062d4538ec6
Gamber-github <wojciech.kubik1993@gmail.com>
2023-09-04 17:41:53 +0200

    esLint changes

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,16 +1,9 @@
 import styled from "styled-components"
 
-<<<<<<< HEAD
 import { RegisterForm } from "../components/Register/RegisterForm"
 import { VerticalBackground } from "../components/ui/Background/VerticalBackground"
 import { TwoColumns } from "../components/ui/TwoColumns/TwoColumns"
 import background from "../images/register_background.jpg"
-=======
-import background from "../../public/register_background.jpg"
-import { RegisterForm } from "../components/Register/RegisterForm"
-import { VerticalBackground } from "../components/ui/Background/VerticalBackground"
-import { TwoColumns } from "../components/ui/TwoColumns/TwoColumns"
->>>>>>> main
 
 export const Register = () => {
   return (
@@ -23,11 +16,7 @@ export const Register = () => {
 const RegisterContainer = styled.div`
   display: flex;
   flex-direction: column;
-<<<<<<< HEAD
   width: 800px;
-=======
-  width: 600px;
->>>>>>> main
   height: 90vh;
   margin: auto auto;
   -webkit-box-shadow: 0px 0px 40px -9px rgba(66, 68, 90, 1);
